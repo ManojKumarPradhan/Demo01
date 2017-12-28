0eeb0b020f82aff716a4d90253387b4416224b96
ManojKumarPradhan <mkpmca123@gmail.com>
2017-12-28 18:20:28 +0530

    Added Clone of Patient Service For Load Balancing

---
+++ Patient-S2/src/main/java/com/bigob/PatientS2Application.java
@@ -0,0 +1,12 @@
+package com.bigob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientS2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientS2Application.class, args);
+	}
+}
