65315d3fb5850c5c9aab851deba87ac2bedc951c
ManojKumarPradhan <mkpmca123@gmail.com>
2017-12-28 18:07:44 +0530

    Revert "Duplicate Patient Service Added with port 8034"
    This reverts commit 33161185ceb5ed3039a9dc562e63627b05e54b25.

--- Patient-S2/src/main/java/com/bigob/util/ServiceConstand.java
+++
@@ -1,6 +0,0 @@
-package com.bigob.util;
-
-public interface ServiceConstand {
-	String DATE_FORMAT="patient.date.format";
-	String PATIENT_BY_NAME="from Patient pat where pat.fName like %"+":name"+"%";
-}
