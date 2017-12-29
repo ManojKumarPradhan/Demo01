b41d7ce2f3e411a448267381a7f63515c24e4c95
ManojKumarPradhan <mkpmca123@gmail.com>
2017-12-29 14:47:44 +0530

    Doctor Service Added
    Doctor Service Added

--- Doctor-S/src/main/java/com/bigob/util/ServiceConst.java
+++
@@ -1,6 +0,0 @@
-package com.bigob.util;
-
-public interface ServiceConst {
-	String DATE_fORMAT="doctor.date.format";
-	String GET_DOCTOR_BY_NAME="from Doctor doc where doc.fName like %"+":name"+"%";
-}
