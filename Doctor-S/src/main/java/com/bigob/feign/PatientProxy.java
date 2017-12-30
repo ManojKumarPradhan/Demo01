00646263c2a27cc5aefe3c5e9ff554bed2856cb3
ManojKumarPradhan <mkpmca123@gmail.com>
2017-12-30 23:12:27 +0530

    EUREKA Server Added

--- Doctor-S/src/main/java/com/bigob/feign/PatientProxy.java
+++ Doctor-S/src/main/java/com/bigob/feign/PatientProxy.java
@@ -1,7 +1,6 @@
 package com.bigob.feign;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bigob.bean.PatientBean;
 
-@RibbonClient
-@FeignClient(name = "patient-proxy")
+@FeignClient(name = "patient")
 public interface PatientProxy {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/patient/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
